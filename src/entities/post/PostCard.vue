2cab1213b05049638c502261dc6f8894b1ad1f6e
Álex Cortiñas <mail@alexcortinas.es>
2021-11-10 16:02:14 +0100

    Metiendo la imagen en el PostCard

--- src/entities/post/PostCard.vue
+++ src/entities/post/PostCard.vue
@@ -1,17 +1,19 @@
 <template>
   <v-card>
-    <v-card-title>
-      <router-link :to="{ name: 'PostDetail', params: { id: post.id } }" v-slot="{ navigate }" custom>
-        <span @click="navigate" class="pointer">{{ post.title }}</span>
-      </router-link>
-      <v-spacer />
-      <span>by {{ post.author.login }}</span>
-    </v-card-title>
-    <v-card-subtitle> {{ post.timestamp }} </v-card-subtitle>
+    <v-img class="blue--text align-end" :src="getImageSrc(post)">
+      <v-card-title>
+        <router-link :to="{ name: 'PostDetail', params: { id: post.id } }" v-slot="{ navigate }" custom>
+          <span @click="navigate" class="pointer">{{ post.title }}</span>
+        </router-link>
+        <v-spacer />
+        <span>by {{ post.author.login }}</span>
+      </v-card-title>
+      <v-card-subtitle> {{ post.timestamp }} </v-card-subtitle>
 
-    <v-card-text v-if="post.tags.length > 0">
-      Tags: <span class="tags">{{ tagsAsString(post.tags) }}</span>
-    </v-card-text>
+      <v-card-text v-if="post.tags.length > 0">
+        Tags: <span class="tags">{{ tagsAsString(post.tags) }}</span>
+      </v-card-text>
+    </v-img>
   </v-card>
 </template>
 
@@ -27,6 +29,12 @@ export default {
   methods: {
     tagsAsString(tags) {
       return tags.map((t) => t.name).join(", ");
+    },
+    getImageSrc(post) {
+      if (post.hasImage) {
+        return `http://localhost:8080/api/posts/${this.post.id}/image`;
+      }
+      return "/placeholder.png";
     },
   },
 };
