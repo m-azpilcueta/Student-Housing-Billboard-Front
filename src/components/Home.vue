4a697ce8381fe71ef0b07e6bd00cf075c54c61fe
martinazpilcueta <m.azpilcueta@udc.es>
2022-01-07 19:27:29 +0100

    Estructura inicial filtrados

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,25 @@
 <template>
   <v-container>
     <v-row>
+      <v-col cols="12">
+        <v-card class="d-flex align-start">
+          <v-card-title class="mr-2">Ordenar por:</v-card-title>
+          <v-select :items="ordenacion" class="mr-5" placeholder="Seleccionar ordenación"></v-select>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="4">
+        <v-card>
+          <v-card-title class="mb-4">Filtrar por:</v-card-title>
+          <v-card-subtitle>Universidad</v-card-subtitle>
+          <v-card-subtitle>Estudios</v-card-subtitle>
+          <v-card-subtitle>Provincia</v-card-subtitle>
+          <v-card-subtitle>Localidad</v-card-subtitle>
+          <v-card-subtitle>Nº de habitaciones</v-card-subtitle>
+          <v-card-subtitle>Nº de personas</v-card-subtitle>
+          <v-card-subtitle>Importe mensual</v-card-subtitle>
+          <v-card-subtitle>Disponibilidad</v-card-subtitle>
+        </v-card>
+      </v-col>
       <v-col cols="12" sm="6" md="4" v-for="piso in pisos" :key="piso.id">
         <PisoCard :piso="piso"></PisoCard>
       </v-col>
@@ -15,6 +34,12 @@ export default {
   data() {
     return {
       pisos: [],
+      ordenacion: [
+        { text: "Importe mensual (menor a mayor)" },
+        { text: "Importe mensual (mayor a menor)" },
+        { text: "Fecha de publicación (descendente)" },
+        { text: "Fecha de publicación (ascendente)" },
+      ],
     };
   },
   mounted() {
@@ -28,3 +53,5 @@ export default {
   },
 };
 </script>
+
+<style scoped></style>
