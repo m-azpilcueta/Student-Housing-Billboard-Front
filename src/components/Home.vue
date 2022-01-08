3cd510588fa52586855d109e7096f728056ec5b4
martinazpilcueta <m.azpilcueta@udc.es>
2022-01-08 03:14:36 +0100

    Añadido mensaje para cuando no existe ningun piso

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,9 +9,9 @@
       </v-col>
       <v-col cols="12" sm="6" md="4">
         <v-card>
-          <v-card-title class="mb-4">Filtrar por:</v-card-title>
-          <v-card-subtitle>Universidad</v-card-subtitle>
-          <v-card-subtitle>Estudios</v-card-subtitle>
+          <v-card-title>Filtrar por:</v-card-title>
+          <v-select class="pr-4 pl-4" label="Universidad"></v-select>
+          <v-select class="pr-4 pl-4" label="Estudios"></v-select>
           <v-card-subtitle>Provincia</v-card-subtitle>
           <v-card-subtitle>Localidad</v-card-subtitle>
           <v-card-subtitle>Nº de habitaciones</v-card-subtitle>
@@ -20,7 +20,10 @@
           <v-card-subtitle>Disponibilidad</v-card-subtitle>
         </v-card>
       </v-col>
-      <v-col cols="12" sm="6" md="4" v-for="piso in pisos" :key="piso.id">
+      <v-col cols="12" sm="6" md="8" v-if="pisos.length === 0" class="d-flex align-center justify-center">
+        <span>Todavía no se ha publicado ningún piso.</span>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" v-for="piso in pisos" :key="piso.idPiso">
         <PisoCard :piso="piso"></PisoCard>
       </v-col>
     </v-row>
